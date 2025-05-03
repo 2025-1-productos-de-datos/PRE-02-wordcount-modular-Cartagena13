c45046b5c7074072ae7d99b525e5861d46a481f7
Alejandro Cartagena <acartagena@unal.edu.co>
2025-05-03 09:53:49 -0500

    correccion funcion de lectura input

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -5,7 +5,7 @@ import os
 def read_all_lines():
     all_lines=[]
     input_files_list=os.listdir('data/input/')
-    for filename in input_file_list:
+    for filename in input_files_list:
         with open(filename, "r", encoding="utf-8") as f:
             lines=f.readlines()
             all_lines.extend(lines)
