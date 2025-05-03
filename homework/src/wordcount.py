b55f7cce6c74ab44144252e90f0f39f044fd56cc
Alejandro Cartagena <acartagena@unal.edu.co>
2025-05-03 11:04:30 -0500

    funcion faltante (preprocess)

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -2,6 +2,7 @@
 import os
 
 from ._internals.count_words import count_words
+from ._internals.preprocess_lines import preprocess_lines
 from ._internals.read_all_lines import read_all_lines
 from ._internals.split_in_words import split_in_words
 from ._internals.write_count_words import write_count_words
@@ -9,23 +10,13 @@ from ._internals.write_count_words import write_count_words
 
 def main():
 
-    ## mover a la funcion read_all_lines
     all_lines = read_all_lines()
 
-
-    ## mover a preprocess_all_lines
-
-    all_lines=[line.lower().strip() for line in all_lines]
-
-
-    ## split in words
+    all_lines = preprocess_lines(all_lines)
 
     words = split_in_words(all_lines)
 
-
-    ## count words
     counter = count_words(words)
-
 
     write_count_words(counter)
 
